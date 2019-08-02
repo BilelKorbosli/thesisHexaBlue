f97265e9f6bf6b0040dbfd8ebd9a5c41194865d7
bilel.korbosli <bilel.kor@gmail.com>
2019-08-02 19:11:02 +0200

    configs and files for cloud heroku.com

--- server/server.js
+++ server/server.js
@@ -1,13 +1,2 @@
-var app = express();
-var path = require('path');
-
-var port     = process.env.PORT || 80;
-
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
-});
-
-app.listen(port);
-console.log("App listening on port " + port);
+var statik = require('statik');
+statik(process.env.PORT || 1337);
